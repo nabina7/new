823b3a06c8c148713f82cedc446d800888f8148f
nabina7 <dreaamgaming67@gmail.com>
2024-08-06 12:48:26 +0545

    Your commit message

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const shoes=['adidas','nike','jodan','zoom']
+shoes[4]=78
+console.log(shoes.length)
+console.log(shoes[3])
+shoes.push('nabina')
+console.log(shoes)
+shoes.unshift('sabina')// add new element at the begining
+console.log(shoes)
+shoes.pop()
+console.log(shoes)
+// array methods
+let num=[1,2,3,4]
+//let b=num.toString()// b is now a string
+console.log(num,typeof num)
+num.pop()
+console.log(num)
+num.shift()// remove element from first
+console.log(num)
+ const isRainig=true
+ if(isRainig){
+    console.log("umbrella boka nabina")
+ }
+ else{
+    console.log("na boka ")
+ }
+
